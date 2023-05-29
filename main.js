9fdb1e481a3c7e344d84bc4743521c9935e376da
ggarmo <ggarmo@users.noreply.github.com>
2023-05-29 16:51:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,59 +4,13 @@ if ('serviceWorker' in navigator) {
   .catch(err => console.log('Service Worker Not Registered', err));
 }
 
-let board = Array(9).fill(null);
-let turn = 'X';
-const boardDiv = document.getElementById('board');
-const resetButton = document.getElementById('reset');
+let greetings = ['Hello', 'Hi', 'Hey', 'Hola', 'Bonjour'];
+let names = ['Emily', 'Sophia', 'Emma', 'Olivia', 'Ava'];
 
-function createBoard() {
-  boardDiv.innerHTML = '';
-  for(let i = 0; i < board.length; i++) {
-    const div = document.createElement('div');
-    div.addEventListener('click', () => makeMove(i));
-    div.textContent = board[i];
-    boardDiv.appendChild(div);
-  }
-}
-
-function makeMove(i) {
-  if(board[i] === null) {
-    board[i] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
-    createBoard();
-    if(checkWinner()) {
-      alert(`${turn === 'X' ? 'O' : 'X'} wins!`);
-      resetGame();
-    }
-  }
-}
-
-function checkWinner() {
-  const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for(let condition of winConditions) {
-    if(board[condition[0]] !== null &&
-       board[condition[0]] === board[condition[1]] &&
-       board[condition[0]] === board[condition[2]]) {
-         return true;
-    }
-  }
-  return false;
-}
-
-function resetGame() {
-  board = Array(9).fill(null);
-  createBoard();
-}
-
-resetButton.addEventListener('click', resetGame);
-createBoard();
+document.getElementById('generate').addEventListener('click', () => {
+  let randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+  let randomName = names[Math.floor(Math.random() * names.length)];
+  let listItem = document.createElement('li');
+  listItem.textContent = `${randomGreeting}, ${randomName}!`;
+  document.getElementById('greetings').appendChild(listItem);
+});
